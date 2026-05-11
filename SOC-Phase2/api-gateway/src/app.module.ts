1cdba3cb7c3f5ddd555d037d06106673133c099b
Anas-ahmed23 <anasbahaa1010@outlook.com>
2026-05-11 13:18:57 +0300

    Task 2 add sessions gateway proxy

--- SOC-Phase2/api-gateway/src/app.module.ts
+++ SOC-Phase2/api-gateway/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProxyService } from './proxy/proxy.service';
+import { SessionsGatewayController } from './sessions/sessions-gateway.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [HttpModule],
+  controllers: [AppController, SessionsGatewayController],
+  providers: [AppService, ProxyService],
 })
 export class AppModule {}
