46c83f611e21d304fa20fe3326e993cbfaa218b8
Anas-ahmed23 <anasbahaa1010@outlook.com>
2026-05-11 13:21:00 +0300

    Task 4 add participants gateway proxy

--- SOC-Phase2/api-gateway/src/app.module.ts
+++ SOC-Phase2/api-gateway/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ParticipantsGatewayController } from './participants/participants-gateway.controller';
 import { ProxyService } from './proxy/proxy.service';
 import { SessionsGatewayController } from './sessions/sessions-gateway.controller';
 import { TemplatesGatewayController } from './templates/templates-gateway.controller';
@@ -10,6 +11,7 @@ import { TemplatesGatewayController } from './templates/templates-gateway.contro
   imports: [HttpModule],
   controllers: [
     AppController,
+    ParticipantsGatewayController,
     SessionsGatewayController,
     TemplatesGatewayController,
   ],
